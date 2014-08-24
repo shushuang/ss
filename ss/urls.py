11d5e91f8d5257aba78ee51a0ec2c0a0f1a655aa
shushuang163@163.com <shushuang163@163.com>
2014-08-25 01:20:51 +0800

    add a home app and a home page for django

--- ss/urls.py
+++ ss/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from home.views import home_page
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home_page),
 )
